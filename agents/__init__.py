9279f2658f63906d1e6c1b5fd57bfe067169ccf8
inf581-pvz-anonymous <80271721+inf581-pvz-anonymous@users.noreply.github.com>
2021-03-08 19:00:09 +0100

    Update __init__.py

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,5 +1,6 @@
 from .reinforce_agent_v2 import PolicyNetV2, ReinforceAgentV2, PlayerV2
 from .ddqn_agent import QNetwork, DDQNAgent, PlayerQ
+from .dqn_agent import QNetwork_DQN, DQNAgent
 
 
 from .threshold import Threshold
