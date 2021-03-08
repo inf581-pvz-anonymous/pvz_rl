137e442970a58d9a8f94fdc32684a229fa818bbc
inf581-pvz-anonymous <80271721+inf581-pvz-anonymous@users.noreply.github.com>
2021-03-08 20:17:58 +0100

    Update __init__.py

--- agents/__init__.py
+++ agents/__init__.py
@@ -5,3 +5,5 @@ from .evaluate_agent import evaluate
 from .reinforce_agent_v2 import PolicyNetV2, ReinforceAgentV2, PlayerV2
 from .ddqn_agent import QNetwork, DDQNAgent, PlayerQ, experienceReplayBuffer
 from .dqn_agent import QNetwork_DQN, DQNAgent
+from .actor_critic_agent import PolicynetAC, ACAgent, TrainerAC
+from .keyboard_agent import KeyboardAgent
