f9f2889a8cd46c4129fa7a0a5953dbbf476bc9dc
inf581-pvz-anonymous <80271721+inf581-pvz-anonymous@users.noreply.github.com>
2021-03-08 20:09:58 +0100

    Update train_policy_agent.py

--- train_policy_agent.py
+++ train_policy_agent.py
@@ -82,7 +82,7 @@ def train(env, agent, n_iter=100000, n_record=500, n_save=1000, n_evaluate=10000
 
 if __name__ == "__main__":
 
-    env = TrainerV2(render=False,max_frames = 400)
+    env = PlayerV2(render=False,max_frames = 400)
     agent = DiscreteAgentV2(
         input_size = env.num_observations(),
         possible_actions=env.get_actions()
